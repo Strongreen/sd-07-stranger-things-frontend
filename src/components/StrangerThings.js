32cb063ef57236cc7579dd5cf121cff4d67c2aa9
Nonato <lucas.nonato1@hotmail.com>
2021-04-28 18:56:43 -0300

    prod remote

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -163,9 +163,9 @@ class StrangerThings extends React.Component {
             <button type="button" onClick={ this.previousPage }>Anterior</button>
             <button type="button" onClick={ this.nextPage }>Próximo</button>
           </div>
-          <footer className="development-text">
+          {/* <footer className="development-text">
             <h4>Em desenvolvimento</h4>
-          </footer>
+          </footer> */}
         </div>
       </div>
     );
