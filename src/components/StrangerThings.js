9b1f3193a838c31cb9a0ef88d9f9b5b21a4e7d6a
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-04-28 19:13:42 -0300

    Configurando

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -5,14 +5,16 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
+const timeoutSet = 30000;
+
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL || 'http://localhost:3002',
-  timeout:process.env.TIMEOUT || 30000,
+  timeout: process.env.TIMEOUT || timeoutSet,
 };
 
 const upsideDownConfig = {
-  url:process.env.REACT_APP_UPSIDEDOWN_URL || 'http://localhost:3003',
-  timeout:process.env.TIMEOUT || 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL || 'http://localhost:3003',
+  timeout: process.env.TIMEOUT || timeoutSet,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
