7d7f134c33387f5cfcca9fcebb5f5bfdb1a3c246
Vitor Rodrigues <vitor_rc@outlook.com>
2021-04-28 21:31:27 -0300

    Req 8

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+require('dotenv').config();
 
 const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = process.env.PUBLIC_URL || 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
