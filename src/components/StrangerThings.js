78d675ee79f6e96503e84effee40683b3b901c11
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-04-28 21:32:52 -0300

    craçao variaveis de ambiante

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { REACT_APP_HAWKINS_URL, REACT_APP_HAWKINS_TIMEOUT, REACT_APP_UPSIDEDOWN_URL } from 'dotenv';
 import CharactersService from '../services/charactersAPI';
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
@@ -6,13 +7,13 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: REACT_APP_HAWKINS_URL,
+  timeout: REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: REACT_APP_UPSIDEDOWN_URL,
+  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
