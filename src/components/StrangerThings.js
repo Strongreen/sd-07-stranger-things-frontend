54bbed8c86bee7160c9423cddddc0295efa3c784
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-29 18:40:41 -0300

    Faltava o dotenv

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 
+require('dotenv').config();
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
@@ -128,7 +130,8 @@ class StrangerThings extends React.Component {
       <div className={ `reality ${getRealityClass(hereIsTheUpsideDownWorld)}` }>
         <div className="content strangerfy">
           <div className="change-reality">
-            {process.env.REACT_APP_DEVELOPMENT === "true" ? <h1>Em desenvolvimento</h1> : null}
+            {process.env.REACT_APP_DEVELOPMENT === "true" && <h1>Em desenvolvimento</h1> }
+            {console.log(process.env.REACT_APP_DEVELOPMENT)}
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
               Mudar de Realidade
