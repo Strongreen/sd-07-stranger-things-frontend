13b1115f2f315436ce18543bf11ba8dfbd7e2a55
Strongreen <strongreen@live.com>
2021-05-08 15:13:22 -0300

    resolvendo gambiarra

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const {
 function App() {
   return (
     <div className="App">
-      { STATUS_APP === undefined ? <h4>Em desenvolvimento</h4> : <StrangerThings />}
+      { STATUS_APP === 'false' ? <h4>Em desenvolvimento</h4> : <StrangerThings />}
     </div>
   );
 }
