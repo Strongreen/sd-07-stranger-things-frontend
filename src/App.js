fdde550322e2ad898af582375a5c398db7c3ea70
Strongreen <strongreen@live.com>
2021-05-08 12:13:47 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 
+require('dotenv').config();
+
+const { STATUS } = process.env;
+
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
+      { STATUS !== 'producao' ?? 'Em desenvolvimento' }
       <StrangerThings />
     </div>
   );
