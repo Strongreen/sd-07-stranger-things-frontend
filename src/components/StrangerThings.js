67f58afcbe2cff0bf2cd0b4c7776c685f9217eab
lisvalver <lisvalver.atte@gmail.com>
2021-05-08 23:55:48 -0300

    deploy - lisvalver-pd

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -130,6 +130,7 @@ class StrangerThings extends React.Component {
     const { hereIsTheUpsideDownWorld, characterName, characters, page } = this.state;
     return (
       <div className={ `reality ${getRealityClass(hereIsTheUpsideDownWorld)}` }>
+        { process.env.REACT_APP_DEVELOPMENT === 'true' && <span>Em desenvolvimento</span>}
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
