06022ed5dcc8f3531c8baa290f8aa112b8611e3d
Marcio <marcio@localhost.localdomain>
2021-06-02 18:28:47 -0300

    9

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -134,6 +134,7 @@ class StrangerThings extends React.Component {
 
   render() {
     const { hereIsTheUpsideDownWorld, characterName, characters, page } = this.state;
+    
     return (
       <div className={ `reality ${getRealityClass(hereIsTheUpsideDownWorld)}` }>
         <div className="content strangerfy">
