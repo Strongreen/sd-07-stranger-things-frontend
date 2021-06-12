900c55fc4d5c386be1893a540f94e635f681d509
jvitorof <j.vitoroflores@gmail.com>
2021-06-12 18:36:01 -0300

    FAMOSO LIT

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -135,7 +135,11 @@ class StrangerThings extends React.Component {
             </button>
           </div>
           <div>
-            { process.env.REACT_APP_DEVELOPMENT === 'true' ? <p>Em desenvolvimento</p> : <p>V1</p> }
+            {
+              process.env.REACT_APP_DEVELOPMENT === 'true'
+              ? <p>Em desenvolvimento</p> 
+              : <p>V1</p>
+            }
           </div>
           <div>
             <input
