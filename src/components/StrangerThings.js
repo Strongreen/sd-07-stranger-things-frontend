97ee4bf5ae3879145780b61e71fb1f3b0bf2758b
Luiz Mariz <lfsmariz@gmail.com>
2021-06-13 19:06:09 -0300

    100%

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -166,6 +166,7 @@ class StrangerThings extends React.Component {
             <button type="button" onClick={ this.nextPage }>Próximo</button>
           </div>
         </div>
+        <h2>Em desenvolvimento</h2>
       </div>
     );
   }
